187c858a13fc2c54cf7f712c6c790c6dc70f3ab0
Marc Tchucky <marc.tchucky@gmail.com>
2014-10-10 17:21:55 +0200

    Added example from Eric A. Meyer awesome S5 slideshare system

---
+++ main.js
@@ -0,0 +1,18 @@
+(function () {
+
+    $(document).ready(onDocumentReady);
+
+    function onDocumentReady() {
+	$('#commandZone .left').click(onCommandLeft);
+	$('#commandZone .right').click(onCommandRight);
+    }
+    
+    function onCommandLeft() {
+	alert('left');
+    }
+    
+    function onCommandRight() {
+	alert('right');
+    }
+    
+})();
