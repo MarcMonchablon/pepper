a5de61fc01e9119c98907b8ba407f50fa3871f9b
Marc Tchucky <marc.tchucky@gmail.com>
2014-10-16 20:27:03 +0200

    Added Browserify task for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp");
 var gutil = require("gulp-util");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
 
 var connect = require("gulp-connect");
 gulp.task('webserver', function() {
@@ -10,31 +12,48 @@ gulp.task('webserver', function() {
     });
 });
 
+
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+gulp.task('browserify', function() {
+    var bundleStream = browserify('./app/app.js').bundle();
+
+    bundleStream
+	.pipe(source('./app/app.js'))
+	.pipe(rename('bundle.js'))
+	.pipe(gulp.dest('./app'));
+});
+
 var jade = require("gulp-jade");
 gulp.task('jade', function() {
-    gulp.src('app/index.jade')
+    gulp.src('app/**/*.jade')
     .pipe(jade({pretty: true}))
     .pipe(gulp.dest('app/'))
     .pipe(connect.reload());
 });
 
+
+
 var stylus = require("gulp-stylus");
+
 gulp.task('stylus', function() {
-    gulp.src('app/css/style.styl')
+    gulp.src('app/**/*.styl')
     .pipe(stylus())
+    .pipe(concat('style.css'))
     .pipe(gulp.dest('app/css/'))
     .pipe(connect.reload());
 });
 
 
 gulp.task('watch', function() {
-    gulp.watch('app/*.jade', ['jade']);
-    gulp.watch('app/css/*.styl', ['stylus']);
+    gulp.watch('app/**/*.jade', ['jade']);
+    gulp.watch('app/**/*.styl', ['stylus']);
+    gulp.watch('app/**/*.js', ['browserify']);
 });
 
 
 
-gulp.task('default', ['jade', 'stylus', 'webserver', 'watch']);
+gulp.task('default', ['jade', 'stylus', 'browserify', 'webserver', 'watch']);
 
 
 
