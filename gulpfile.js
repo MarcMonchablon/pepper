6fc6aa4fce57653010bc47f3060e219aab60677a
Marc Tchucky <marc.tchucky@gmail.com>
2014-10-29 23:31:21 +0100

    Removed useless comments in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -55,34 +55,3 @@ gulp.task('watch', function() {
 
 
 gulp.task('default', ['jade', 'stylus', 'browserify', 'webserver', 'watch']);
-
-
-
-/*
-var webserver = require("gulp-webserver");
-gulp.task('webserver', function() {
-    gulp.src('app')
-    .pipe(webserver({
-    livereload: true,
-    directoryListing: false,
-    port: 8555}));
-});
-
-
-var jade = require("gulp-jade");
-gulp.task('jade', function() {
-    gulp.src('app/index.jade')
-    .pipe(jade({pretty: true}))
-    .pipe(gulp.dest('app/'))
-    .on('error', gutil.log);
-});
-
-var stylus = require("gulp-stylus");
-gulp.task('stylus', function() {
-    gulp.src('app/css/style.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('app/css/'))
-    .on('error', gutil.log);
-});
-
-gulp.task('compile', ['jade', 'stylus']); */
